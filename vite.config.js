28d997e5450b5414677e86a04bddf3a73c99bfaa
Hammas Rashid <hammas.coding.46@gmail.com>
2024-11-07 23:25:36 +0500

    almostdone

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,48 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['vite.svg', 'robots.txt', 'hunt-weather.png'],
+      registerType: "autoUpdate",
+      includeAssets: [
+        "hunt-weather.webp",
+        "vite.svg",
+        "assets/cloudy.png",
+        "assets/hard-storm.png",
+        "assets/humidity.png",
+        "assets/night.png",
+        "assets/partly-cloudy.png",
+        "assets/rain.png",
+        "assets/snow.png",
+        "assets/storm.png",
+        "assets/sun.png",
+        "assets/temperature.png",
+        "assets/wallpaper.jpg",
+        "assets/wind-speed.png",
+      ],
       manifest: {
-        name: 'My Awesome React App',
-        short_name: 'MyApp',
-        description: 'An amazing PWA built with Vite and React',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
+        name: "Weather App",
+        short_name: "Weather",
+        description: "A Progressive Web App for weather updates",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "hunt-weather.webp",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
+            src: "hunt-weather.webp",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
